c4551da24080bc8b67617679fb4906e63f515b0f
Andrés Pozo <andres.pozom@gmail.com>
2015-10-18 18:45:09 +0200

    Fixing some indentation issues

--- guybrush/image.py
+++ guybrush/image.py
@@ -6,7 +6,7 @@ import sys
 
 class Image:
 
-	def __init__(self, stringOfFileWithSrcImage, textStartCoordinate=(50,50)):
+    def __init__(self, stringOfFileWithSrcImage, textStartCoordinate):
         try:
             self.fileWithImage = stringOfFileWithSrcImage
             self.img = Image.open(stringOfFileWithSrcImage)
@@ -14,21 +14,23 @@ class Image:
         except:
             self.img = ''
             e = sys.exc_info()[0]
-			print "Error: " + str(e) 
+            print "Error: " + str(e) 
             sys.exit(2)
 
     def saveImageToFile(self, stringOfFileToSave):
-    	try:
-    		self.img.save(stringOfFileToSave)
-    	except:
-    		e = sys.exc_info()[0]
-			print "Error: " + str(e) 
-    		sys.exit(3)
+        try:
+    	    self.img.save(stringOfFileToSave)
+        except:
+            e = sys.exc_info()[0]
+            print "Error: " + str(e) 
+            sys.exit(3)
 
     def pasteRegion(self, regionToPaste):
-    	try:
-    		self.img.paste(regionToPaste,self.textStartCoordinate,regionToPaste)
-		except:
-    		e = sys.exc_info()[0]
-			print "Error: " + str(e) 
-			exit(4) 
+        try:
+            self.img.paste(regionToPaste,self.textStartCoordinate,regionToPaste)
+            (width, heigth)=regionToPaste.regionSize
+            self.textStartCoordinate = (self.textStartCoordinate[0]+width, self.textStartCoordinate[1])
+        except:
+            e = sys.exc_info()[0]
+            print "Error: " + str(e) 
+            exit(4) 
